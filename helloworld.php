1fa1544dcb816964a0eda246943ef2f7d9becf2a
LAPTOP-6J3AQV8C\Mehme <97094331@st.deltion.nl>
2022-03-31 11:19:25 +0200

    Versie 4

--- helloworld.php
+++ helloworld.php
@@ -1,14 +1,14 @@
 <?php
 
-echo "Hello Wereld\n" ;
+echo "Hello Wereld!\n" ;
 
 ?>
 
 <?php
 
-echo "Hello World\n";
-echo "Hello World\n";
-echo "Hello World\n";
-echo "Hello World\n";
+echo "Hello World!\n";
+echo "Hello World!\n";
+echo "Hello World!\n";
+echo "Hello World!\n";
 
 ?>
